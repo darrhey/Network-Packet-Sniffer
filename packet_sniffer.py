131237b9fa9624b400bfb79128de737c35a95752
darrhey <35179225+darrhey@users.noreply.github.com>
2025-06-05 19:33:16 +0100

    Create packet_sniffer.py

---
+++ packet_sniffer.py
@@ -0,0 +1,14 @@
+import socket
+import struct
+
+
+# Unpack ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac),socket.htons(proto), data[14:]
+
+# Return formatted MAC
+def get_mac_addr(bytes_addr):
+    bytes_str=map('{:02x}'.format, bytes_addr)
+    mac_addr = ':'.join(bytes_str).upper()
+    return mac_addr
